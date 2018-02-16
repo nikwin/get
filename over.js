6bc40164b5964fffcf019e4a2be0df552a070797
Nikhil Murthy <murthynik1@gmail.com>
2018-02-16 13:03:11 +0530

    Put in the struggle and fixed the ledge collisions.

--- over.js
+++ over.js
@@ -1,5 +1,5 @@
 var Protag = function(){
-    this.rect = [100, height - 110, 100, 200];
+    this.rect = [100, height - 205, 100, 200];
     this.vel = [0, 0];
     this.image = 'protag';
 };
@@ -10,7 +10,7 @@ Protag.prototype.draw = function(){
 
 Protag.prototype.update = function(interval, ledges){
     if (this.vel[1]){
-        this.vel[1] -= 0.3;
+        this.vel[1] += 0.3;
     }
 
     this.rect[0] += this.vel[0];
@@ -26,9 +26,9 @@ Protag.prototype.update = function(interval, ledges){
     }
 
     _.each(ledges, ledge => {
-        if (collideRect(this.rect, ledge.rect) && this.vel[1] > 0 && this.rect[1] + this.rect[3] < ledge.rect[1] + 5){
+        if (collideRect(this.rect, ledge.rect) && this.vel[1] > 0 && this.rect[1] + this.rect[3] - this.vel[1] < ledge.rect[1]){
             this.vel[1] *= -0.25;
-            if (this.vel[1] < 0.5){
+            if (this.vel[1] > -0.5){
                 this.vel = [0, 0];
             }
             this.rect[1] = ledge.rect[1] - this.rect[3] - 1;
@@ -43,6 +43,26 @@ var Ledge = function(image, rect){
 
 Ledge.prototype.draw = Protag.prototype.draw;
 
+var StruggleButton = function(protag){
+    this.protag = protag;
+    this.rect = [width - 120, 20, 100, 40];
+    this.name = 'Struggle';
+};
+
+StruggleButton.prototype.clicked = function(){
+    if (Math.random() < 0.25 && !_.any(this.protag.vel, _.identity)){
+        this.protag.vel[1] = -10 - Math.random() * 5;
+        this.protag.vel[0] = (10 + Math.random() * 5) * ((Math.random() > 0.5) ? 1 : -1);
+    }
+};
+
+StruggleButton.prototype.draw = function(){
+    ctx.strokeStyle = '#000000';
+    ctxRoundedRect(this.rect);
+    ctx.fillStyle = '#000000';
+    ctx.fillText('STRUGGLE', this.rect[0], this.rect[1] + this.rect[3]);
+};
+
 var Game = function(){
 };
 
@@ -51,10 +71,15 @@ Game.prototype.initialize = function(){
     this.ledges = [
         new Ledge('ground', [0, height - 5, width, 5])
     ];
+    this.buttons = [
+        new StruggleButton(this.protag)
+    ];
+
+    bindHandler.bindFunction(this.getTouchFunction());
 };
 
 Game.prototype.update = function(interval){
-    this.protag.update(interval);
+    this.protag.update(interval, this.ledges);
     return true;
 };
 
@@ -63,6 +88,21 @@ Game.prototype.draw = function(){
     ctx.fillRect(0, 0, width, height);
     this.protag.draw();
     _.each(this.ledges, ledge => ledge.draw());
+    _.each(this.buttons, button => button.draw());
+};
+
+Game.prototype.getTouchFunction = function(){
+    return (e) => {
+        e.preventDefault();
+
+        var pos = getPos(e);
+        
+        _.each(this.buttons, button => {
+            if (containsPos(button.rect, pos)){
+                button.clicked();
+            }
+        });
+    };
 };
 
 var getGame = function(gameProperties){
